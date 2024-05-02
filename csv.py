7760f1f6f36d23d2c257f29ba6566b0427fcf17f
main <gari-vlad-2005@yandex.ru>
2024-05-03 01:52:02 +0900

    костыльки

--- csv.py
+++ csv.py
@@ -1,38 +1,68 @@
 import sqlite3
 
 
-def insert(data, table=False, file_name='data/data.db'):
+def insert(data, table=False, file_name='data/data.db', read_timer=False, add=False, delete=False):
     try:
         # подключение бд
         sqlite_connection = sqlite3.connect(file_name)
         cursor = sqlite_connection.cursor()
         print("Подключен к SQLite")
 
-        # создание столбиков и маски вставки значений
-        if table == 'pictures':
-            param = 'id, name, date, description, url, download'
-            sqlite_insert_with_param = f"""CREATE TABLE IF NOT EXISTS {table}({param});"""
-            cursor.execute(sqlite_insert_with_param)
-        elif table == 'users':
-            param = 'id UNIQUE, nickname, username, date, language, last_send'
-            sqlite_insert_with_param = f"""CREATE TABLE IF NOT EXISTS {table}({param});"""
-            cursor.execute(sqlite_insert_with_param)
-            param = 'id, nickname, username, date, language, last_send'
+        if read_timer:
+            print('чтение расписания...')
+            # Выбираем всех пользователей
+            cursor.execute('SELECT * FROM users')
+            users = cursor.fetchall()
 
-        # проверка наличия записи
-        marks_ids = [x[0] for x in cursor.execute(f"select id from {table}").fetchall()]
-        if data[0] in marks_ids:
-            buf = ', '.join(map(lambda x: str(x) + ' = ?', param.split(', ')))
-            sqlite_insert_with_param = f'UPDATE {table} SET {buf} WHERE id = {data[0]}'
-            print('обновление записи...')
+            # Выводим результаты
+            for user in users:
+                if data[0] == user[0]:
+                    return data[-1]
+        elif add:
+            timers = [cursor.execute(f"select id, timer from users").fetchall()]
+            for timer in timers:
+                if data[0] == timer[0] and timer[1]:
+                    add_point = timer[1]
+                    add_point.append(add)
+                    cursor.execute(f'UPDATE users SET timer = {add_point} WHERE id = {data[0]};')
+                elif data[0] == timer[0] and not timer[1]:
+                    cursor.execute(f'UPDATE users SET timer = {list()} WHERE id = {data[0]};')
+        elif delete:
+            timers = [cursor.execute(f"select id, timer from users").fetchall()]
+            for timer in timers:
+                if data[0] == timer[0] and timer[1]:
+                    add_point = timer[1]
+                    add_point.remove(delete)
+                    cursor.execute(f'UPDATE users SET timer = {add_point} WHERE id = {data[0]};')
+                elif data[0] == timer[0] and not timer[1]:
+                    cursor.execute(f'UPDATE users SET timer = {list()} WHERE id = {data[0]};')
         else:
-            sqlite_insert_with_param = f'''INSERT INTO {table}({param}) 
-                    VALUES({', '.join(['?'] * len(param.split(',')))});'''
-            print('новая запись...')
+            # создание столбиков и маски вставки значений
+            if table == 'pictures':
+                param = 'id, name, date, description, url, download'
+                sqlite_insert_with_param = f"""CREATE TABLE IF NOT EXISTS {table}({param});"""
+                cursor.execute(sqlite_insert_with_param)
 
-        # применение шаблона
-        cursor.execute(sqlite_insert_with_param, data)
-        sqlite_connection.commit()
+            elif table == 'users':
+                param = f'id UNIQUE, nickname, username, date, language, last_send, timer'
+                sqlite_insert_with_param = f"""CREATE TABLE IF NOT EXISTS {table}({param});"""
+                cursor.execute(sqlite_insert_with_param)
+                param = 'id, nickname, username, date, language, last_send'
+
+            # проверка наличия записи
+            marks_ids = [x[0] for x in cursor.execute(f"select id from {table}").fetchall()]
+            if data[0] in marks_ids:
+                buf = ', '.join(map(lambda x: str(x) + ' = ?', param.split(', ')))
+                sqlite_insert_with_param = f'UPDATE {table} SET {buf} WHERE id = {data[0]}'
+                print('обновление записи...')
+            else:
+                sqlite_insert_with_param = f'''INSERT INTO {table}({param}) 
+                        VALUES({', '.join(['?'] * len(param.split(',')))});'''
+                print('новая запись...')
+
+            # применение шаблона
+            cursor.execute(sqlite_insert_with_param, data)
+            sqlite_connection.commit()
         print(f"операция прошла успешно, таблица: {file_name}")
         cursor.close()
 
--- main.py
+++ main.py
@@ -22,12 +22,15 @@ class Bot:
         self.application.add_handler(CommandHandler("forum", self.open_site))
         self.application.add_handler(CommandHandler("random_image", self.random_image))
 
+        self.application.add_handler(CommandHandler("set_timer", self.set_timer))
+        self.application.add_handler(CommandHandler("unset_timer", self.unset))
+
         text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, self.echo)
         self.application.add_handler(text_handler)
 
         # создание главной клавы
         self.btn_keyboard = [
-            ['/help'], ['/close', '/forum', '/random_image']
+            ['/help', '/unset_timer'], ['/close', '/forum', '/random_image']
         ]
         self.markup_btns = ReplyKeyboardMarkup(self.btn_keyboard, one_time_keyboard=False, resize_keyboard=True)
 
@@ -63,7 +66,7 @@ class Bot:
         await update.message.reply_text('я получил - ' + update.message.text)
 
     async def help(self, update, context):
-        await update.message.reply_text('тут будет инструкция к командам)')
+        await update.message.reply_text('/set_timer мин час - ставит таймер, 2 аргумента обязательны')
 
     async def close_keyboard(self, update, context):
         await update.message.reply_text(
@@ -87,6 +90,46 @@ class Bot:
         print(update.message.chat.id)
         await update.message.reply_text("лови картинку:")
         await context.bot.send_photo(update.message.chat.id, url)
+
+    def remove_job_if_exists(self, name, context):
+        """Удаляем задачу по имени.
+        Возвращаем True если задача была успешно удалена."""
+        current_jobs = context.job_queue.get_jobs_by_name(name)
+        if not current_jobs:
+            return False
+        for job in current_jobs:
+            job.schedule_removal()
+        return True
+
+    async def task(self, context, text='проснись!'):
+        """Выводит сообщение"""
+        await context.bot.send_message(context.job.chat_id, text=text)
+
+    async def set_timer(self, update, context):
+        info = update.message.text.split()[1:]
+        time = 60 * int(info[0]) + int(info[1]) * 3600
+        chat_id = update.effective_message.chat_id
+        job_removed = self.remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(self.task, time, chat_id=chat_id, name=str(chat_id), data=time)
+
+
+        text = f'таймер поставлен'
+        if job_removed:
+            text += ' Старая задача удалена.'
+        await update.effective_message.reply_text(text)
+
+    async def unset(self, update, context):
+        """Удаляет задачу, если пользователь передумал"""
+        chat_id = update.message.chat_id
+        job_removed = self.remove_job_if_exists(str(chat_id), context)
+        text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+        await update.message.reply_text(text)
+
+#    async def set_timer(self, update, context):
+#        timers = insert((update.message.chat.id, update.message.chat.first_name, update.message.chat.username,
+#                update.message.date, update.message.from_user.language_code, update.message.text), read_timer=True)
+#        await update.message.reply_text(timers)
+#        await update.message.reply_text("точка таймера создана")
 
 
 # врубаем все
